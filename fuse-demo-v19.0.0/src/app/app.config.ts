b80fd51bfdc0fd9de42f1751bb449e338e761614
Claudiu-Marian Chelcea <claudiu-marian.chelcea@ubisoft.com>
2024-04-07 09:35:53 +0300

    remove layouts

--- fuse-demo-v19.0.0/src/app/app.config.ts
+++ fuse-demo-v19.0.0/src/app/app.config.ts
@@ -101,7 +101,7 @@ export const appConfig: ApplicationConfig = {
                 services: mockApiServices,
             },
             fuse   : {
-                layout : 'classy',
+                layout : 'compact',
                 scheme : 'light',
                 screens: {
                     sm: '600px',
